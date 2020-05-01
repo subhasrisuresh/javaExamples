7482dea8fbef41d7866825c245a93a3a377f599c
Suresh <subhasri.suresh@rinmac1034.lan>
2020-05-01 18:14:41 +0530

    method added

--- java/Example/Inheritance/ClassB.java
+++ java/Example/Inheritance/ClassB.java
@@ -1,5 +1,12 @@
 class ClassB{
-  ClassB{
+  int a;
+  ClassB(int a){
+    this.a = a;
     System.out.println("welcome to class B");
   }
+  void printValue(){
+    System.out.println("value of a is "+ a);
+
+  }
+
 }
